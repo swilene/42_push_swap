ce642dfd898e2f04b6f325d61bf0c2a8712e6bb4
Solene AGUESSE <saguesse@e1r4p5.clusters.42paris.fr>
2022-07-05 12:47:33 +0200

    july05

--- push_swap.c
+++ push_swap.c
@@ -6,24 +6,41 @@
 /*   By: saguesse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 18:52:26 by saguesse          #+#    #+#             */
-/*   Updated: 2022/07/03 19:02:20 by saguesse         ###   ########.fr       */
+/*   Updated: 2022/07/05 12:45:54 by saguesse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <push_swap.h>
+#include "push_swap.h"
 
-int	ft_checkargv()
+int	ft_checkargv(char *str)
 {
-	
+	int	j;
+
+	j = 0;
+	while (str[j])
+	{
+		printf("str[%d] = %c\n", j, str[j]);
+		if (str[j] <= '0' && str[j] >= '9' && str[j] != ' ')
+			return (1);
+		j++;
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
+	int	i;
+
+	i = 1;
 	if (argc == 1)
 	{
-		ft_putstr("Error\n");
+		ft_putstr_fd("Error\n", 1);
 		return (1);
 	}
-	ft_checkargv() == 
+	while (i < argc)
+	{
+		printf("%d\n", ft_checkargv(argv[i]));
+		i++;
+	}
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: saguesse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 18:44:36 by saguesse          #+#    #+#             */
-/*   Updated: 2022/07/03 18:46:37 by saguesse         ###   ########.fr       */
+/*   Updated: 2022/07/05 11:10:27 by saguesse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,16 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <limits.h>
 
+typedef struct s_list
+{
+	int	nb;
+	struct s_list 	*next;
+}		t_list;
+
+void	ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *s);
 
 
 #endif
