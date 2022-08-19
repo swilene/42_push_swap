f5e0bbfb8b2a0099bbb84706052749f247bd07df
swilene <102738831+swilene@users.noreply.github.com>
2022-08-19 15:29:50 +0200

    Add files via upload

---
+++ ft_lstmin.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmin.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saguesse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 15:04:22 by saguesse          #+#    #+#             */
+/*   Updated: 2022/08/19 15:09:46 by saguesse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_lstmin(t_list **lst)
+{
+	t_list	*tmp;
+	int	min;
+
+	tmp = (*lst);
+	min = tmp->nb;
+	while (tmp)
+	{
+		if (min > tmp->nb)
+			min = tmp->nb;
+		tmp = tmp->next;
+	}
+	return (min);
+}
